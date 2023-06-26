ee1e437d810560b8741372320dee06fcb793baae
il2407 <idolev53@gmail.com>
2023-06-26 13:39:16 +0300

    Gw details functions commit

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ import express = require('express');
 import bodyParser = require('body-parser');
 import usecaseRoutes from './src/routes/usecaseRoutes';
 import onGoingRouter from './src/routes/OnGoingRoutes';
+import gatewayRouter from './src/routes/GatewayDetailsRoutes';
 
 const app: Application = express();
 const port = 3000;
@@ -12,6 +13,7 @@ const mongodb_url = 'mongodb+srv://idolev:il2407@supportplatformdb.tcdb8n4.mongo
 app.use(bodyParser.json());
 app.use('/usecase', usecaseRoutes);
 app.use('/on-going', onGoingRouter);
+app.use('/gw-details', gatewayRouter);
 
 mongoose
   .connect(mongodb_url)
@@ -19,7 +21,7 @@ mongoose
     console.log('Connected to MongoDB');
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
-    });
+    });  
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB', error);
